f7517d92f9fee8daf422d8837c3a4adf5cc85cb6
Andrew Li <andrewli357@gmail.com>
2020-03-31 22:03:44 -0500

    Refactor now deployment, remove unused dependencies

---
+++ api/graphql.ts
@@ -0,0 +1,4 @@
+import apolloServer from '../src/server';
+
+const handler = apolloServer.createHandler();
+export default handler;
--- api/index.ts
+++
@@ -1,8 +0,0 @@
-import apolloServer from '../src/server';
-import microCors from 'micro-cors';
-
-const handler = apolloServer.createHandler();
-const cors = microCors();
-const server = cors((req, res) => (req.method === 'OPTIONS' ? res.end() : handler(req, res)));
-
-export default server;
